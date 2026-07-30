60b736a294cc741743b4be24117e3f15d3d0f215
elievasquez <eliecervasquez@gmail.com>
2026-07-30 12:19:37 -0400

    Add configuration for RSS feeds

---
+++ js/config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+  // Agrega o elimina fuentes aquí fácilmente
+  rssFeeds: [
+    { name: "Google Noticias (Ciencia y Tec)", url: "https://news.google.com/rss/headlines/section/topic/SCITECH?hl=es-419&gl=CL&ceid=CL:es-419" },
+    { name: "FayerWayer", url: "https://www.fayerwayer.com/arc/outboundfeeds/rss/" },
+    { name: "Xataka", url: "https://www.xataka.com/feedburner.xml" }
+  ]
+};
