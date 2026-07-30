d788899baddd0605a431a5c91fbc1c3bc92b3d24
elievasquez <eliecervasquez@gmail.com>
2026-07-30 12:20:14 -0400

    Add quotes array with various authors

---
+++ js/quotes.js
@@ -0,0 +1,6 @@
+const QUOTES = [
+  { text: "El caos es un orden aún no descifrado.", author: "José Saramago" },
+  { text: "Que la Fuerza te acompañe.", author: "Star Wars" },
+  { text: "No hay atajos para ningún lugar al que valga la pena ir.", author: "Beverly Sills" },
+  { text: "Hablar es barato. Muéstrame el código.", author: "Linus Torvalds" }
+];
